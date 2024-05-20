af0a5be59fbc2758980b8791d32c69919a40c18d
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-05-20 20:28:11 +0200

    created layout display and styled aside component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,14 +1,47 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
 import { ProductComponent } from './products/product.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CustomerComponent } from './customer/customer.component';
+import { OrderComponent } from './order/order.component';
 
 export const routes: Routes = [
-    {
-        path:"",
-        component: LayoutComponent,
-      },
-  /*     {
-        path:"/product",
-        component: ProductComponent,
-      }, */
+  {
+    path: '',
+    component: LayoutComponent,
+    // children: [
+    //   {
+    //     path: 'overview',
+    //     component: DashboardComponent,
+    //   },
+    //   {
+    //     path: 'dashboard',
+    //     redirectTo: '',
+    //   },
+    //   {
+    //     path: 'customer',
+    //     component: CustomerComponent,
+    //   },
+    //   {
+    //     path: 'order',
+    //     component: OrderComponent,
+    //   },
+    //   {
+    //     path: 'product',
+    //     component: ProductComponent,
+    //   },
+    // ],
+  },
+  // {
+  //   path: 'login',
+  //   component: Login,
+  // },
+  // {
+  //   path: 'register',
+  //   component: RegisterComponent,
+  // },
+  // {
+  //   path: '**',
+  //   component: NotfoundpageComponent,
+  // },
 ];
--- client/src/app/layout/layout/layout.component.ts
+++ client/src/app/layout/layout/layout.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
-import { ProductComponent } from "../../products/product.component";
+import { ProductComponent } from '../../products/product.component';
+import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from '../aside/aside.component';
+import { HeaderComponent } from '../header/header.component';
+import { DashboardComponent } from '../../dashboard/dashboard.component';
 
 @Component({
-    selector: 'app-layout',
-    standalone: true,
-    templateUrl: './layout.component.html',
-    styleUrl: './layout.component.scss',
-    imports: [ProductComponent]
+  selector: 'app-layout',
+  standalone: true,
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.scss',
+  imports: [RouterOutlet, AsideComponent, HeaderComponent, DashboardComponent],
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
