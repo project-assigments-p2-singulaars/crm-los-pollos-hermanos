1bc0cdb001db7ca084cc8e3309dee5f562d92e24
laurabenavides.design@gmail.com <laurabenavides.design@gmail.com>
2024-05-21 13:50:43 +0200

    Created the landing page forms, their logic and added some styles

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout/layout/layout.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
     {
         path:"",
         component: LayoutComponent,
+        
       },
+      {
+        path:"login",
+        component: LandingPageComponent,
+      }
 ];
--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component,OnInit,inject } from '@angular/core';
+import {FormBuilder,FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent {
-
+  //private loginService = inject(AuthService);
+  private formBuilder = inject(FormBuilder);
+  loginForm!: FormGroup;
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      name: ['',Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  submit() {
+    // const user:User = {
+    //   email: this.loginForm.controls['email'].value,
+    //   password: this.loginForm.controls['password'].value
+    // }
+    // if(this.loginForm.valid){
+    //   console.log("form is valid");
+    //   //this.loginService.login(user);
+    //   console.log("the user is ", user )
+    // }
+  }
 }
