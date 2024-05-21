5664af046b0e944d36ac697cd28e3a291353c398
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-05-21 15:51:56 +0200

    created user interface

--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -1,17 +1,30 @@
-import { Component,OnInit,inject } from '@angular/core';
-import {FormBuilder,FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
-import {MatTabsModule} from '@angular/material/tabs';
+import { Component, OnInit, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { User } from '../shared/interfaces/user';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
-  imports: [ReactiveFormsModule,MatFormFieldModule, MatInputModule, MatIconModule,MatButtonModule,MatTabsModule],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule,
+  ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrl: './landing-page.component.scss',
 })
 export class LandingPageComponent {
   //private loginService = inject(AuthService);
@@ -25,17 +38,15 @@ export class LandingPageComponent {
     });
   }
   submit() {
-    // const user:User = {
-    //   email: this.loginForm.controls['email'].value,
-    //   password: this.loginForm.controls['password'].value
-    // }
-    // if(this.loginForm.valid){
-    //   console.log("form is valid");
-    //   //this.loginService.login(user);
-    //   console.log("the user is ", user )
-    // }
+    const user: User = {
+      email: this.loginForm.controls['email'].value,
+      password: this.loginForm.controls['password'].value,
+    };
+    if (this.loginForm.valid) {
+      console.log('form is valid');
+      //this.loginService.login(user);
+      console.log('the user is ', user);
+    }
   }
-  register(){
-    
-  }
+  register() {}
 }
