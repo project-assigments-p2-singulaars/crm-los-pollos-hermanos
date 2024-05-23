9c198759b63b9e9ec09a6fc43852961e4f0678cd
laurabenavides.design@gmail.com <laurabenavides.design@gmail.com>
2024-05-23 15:27:42 +0200

    Create customers list and form

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { LayoutComponent } from './layout/layout/layout.component';
 import { ProductComponent } from './products/product.component';
 import { ProductDetailComponent } from './products/product-detail/product-detail.component';
 import { ProductListComponent } from './products/product-list/product-list.component';
+import { CustomerComponent } from './customer/customer.component';
 
 export const routes: Routes = [
   {
@@ -17,4 +18,8 @@ export const routes: Routes = [
     path: 'products/:id',
     component: ProductDetailComponent,
   },
+  {
+    path: 'customers',
+    component: CustomerComponent,
+  }
 ];
--- client/src/app/shared/customer.service.ts
+++ client/src/app/shared/customer.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError, firstValueFrom, of } from 'rxjs';
+import { Customer } from './interfaces/customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  products!: Customer[];
+  /*   public products = signal<Product[]>([]); */
+  http = inject(HttpClient);
 
-  constructor() { }
+  getAllCustomers() {
+    return this.http.get<Customer[]>('http://localhost:3000/customers');
+  }
+
+  async addCustomer(customer: Customer){
+    console.log(customer);
+    const result = await firstValueFrom(
+      this.http.post<Customer>('http://localhost:3000/customers', customer)
+    )
+        console.log(result);
+  }
+
+// getProductById(id: number):any{
+//   console.log(id)
+//   return this.http.get<Customer>(`http://localhost:3000/products/${id}`); // string interpolation  ${ }
+// }
+  constructor() {}
 }
+
+// async login(credentials:User){
+//     try{
+//       const result = await firstValueFrom(this.http.post<LoginResponseType>(this.url.concat('/login'), credentials))
+
+//       const {user} = result;
+//       this.localStorageService.setItem('user',JSON.stringify(user))
+
+//     }catch(e){
+//       throw e;
+//     }
+//   }
+// }
+
+// addHero(hero: Hero): Observable<Hero> {
+//   return this.http.post<Hero>(this.heroesUrl, hero, httpOptions)
+//     .pipe(
+//       catchError(this.handleError('addHero', hero))
+//     );
+// }
